2b0e8a31e50af1a25198593e151758e97710835a
Max Hallinan <maxhallinan@riseup.net>
2018-06-14 08:28:49 +0200

    Add Docker scripts to ec2-hello-world

--- ec2-hello-world/scripts/run-image.sh
+++ ec2-hello-world/scripts/run-image.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-BASE_TAG='maxhallinan/ec2-hello-world'
-GIT_TAG="$BASE_TAG:$(git rev-parse --short HEAD)"
-LATEST_TAG="$BASE_TAG:latest"
-docker build --tag $TAG .
-docker tag $TAG $LATEST_TAG
-docker push $GIT_TAG
-docker push $LATEST_TAG
+docker run \
+  --publish 3000:3000 \
+  maxhallinan/ec2-hello-world
