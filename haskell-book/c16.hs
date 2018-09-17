654c1ce83f1d72eb2d254b249bea37ec2797fdb2
Max Hallinan <maxhallinan@riseup.net>
2018-09-17 16:59:08 +0200

    Add notes on rank-n types

--- haskell-book/c16.hs
+++ haskell-book/c16.hs
@@ -1,7 +1,91 @@
 module C16 where
 
 
-data Foo = Foo | Bar | Baz deriving (Eq, Show)
+-- data Foo = Foo | Bar | Baz deriving (Eq, Show)
 
-instance Functor Foo where
-  fmap = undefined
+-- instance Functor Foo where
+--   fmap = undefined
+
+data FixMePls a =
+    FixMe
+  | Pls a
+  deriving (Eq, Show)
+
+instance Functor FixMePls where
+  fmap _ FixMe = FixMe
+  fmap f (Pls a) = Pls (f a)
+
+data WhoCares a =
+    ItDoesnt
+  | Matter a
+  | WhatThisIsCalled
+  deriving (Eq, Show)
+
+instance Functor WhoCares where
+  fmap _ ItDoesnt = ItDoesnt
+  fmap _ WhatThisIsCalled = WhatThisIsCalled
+  fmap f (Matter a) = Matter (f a)
+
+replaceWithP :: a -> Char
+replaceWithP = const 'p'
+
+newtype Identity a = Identity a
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+data Pair a = Pair a a 
+
+instance Functor Pair where
+  fmap f (Pair a1 a2) = Pair (f a1) (f a2)
+
+data Two a b = Two a b
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+data Three a b c = Three a b c
+
+instance Functor (Three a b) where
+  fmap f (Three x y z) = Three x y (f z)
+
+data Three' a b = Three' a b b
+
+instance Functor (Three' a) where
+  fmap f (Three' a b1 b2) = Three' a (f b1) (f b2)
+
+data Four a b c d = Four a b c d
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+data Four' a b = Four' a a a b
+
+instance Functor (Four' a) where
+  fmap f (Four' a1 a2 a3 b) = Four' a1 a2 a3 (f b)
+
+data Possibly a = Nope | Yep a deriving (Eq, Show)
+
+instance Functor Possibly where
+  fmap _ Nope = Nope
+  fmap f (Yep a) = Yep $ f a
+
+data Sum a b = First a | Second b deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second $ f b
+
+newtype Constant a b = Constant { getConstant :: a } deriving (Eq, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant v) = Constant v
+
+data Wrap a b = Wrap (a b) deriving (Eq, Show)
+
+instance Functor a => Functor (Wrap a) where
+  fmap f (Wrap aB) = Wrap (fmap f aB)
+
+wrapOne = Wrap (Just 1) 
+
+wrapTwo = fmap (+ 1) wrapOne
