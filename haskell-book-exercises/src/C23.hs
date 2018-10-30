c73008a1047db5e6e4009fa7743229b039940ab6
Max Hallinan <maxhallinan@riseup.net>
2018-10-30 10:19:27 +0100

    Add Haskell notes

--- haskell-book-exercises/src/C23.hs
+++ haskell-book-exercises/src/C23.hs
@@ -86,5 +86,84 @@ infiniteDie = repeat <$> rollDie2
 -- `evalState` discards the final state value.
 -- so none of this state exists until evalState is called
 -- the evaluation is lazy 
-evalState :: State state value -> state -> value
-evalState m s = fst (runState m s)
+-- evalState :: State state value -> state -> value
+-- evalState m s = fst (runState m s)
+
+-- StdGen is (Int32, Int32)
+-- mkStdGen :: Int -> StdGen
+-- next :: g -> (Int, g) where g is a value of type StdGen
+--
+-- random :: (RandomGen g, Random a) => g -> (a, g)
+--
+-- System.Random has two ways of generating a random value:
+-- - provide a seed value
+-- - use the system-initialized generator
+-- StdGen is a datatype from the library
+-- StdGen is a product of two Int32 values
+-- data StdGen = StdGen !Int32 !Int32
+-- These Int32 values are seed values used to generate the next random number
+-- `mkStdGen :: Int -> StdGen`
+-- used to create an initial generator
+-- maps an Int to a generator
+-- `next :: g -> (Int, g)` 
+-- Int in (Int, g) is a pseudorandom number
+-- g is a value of type `StdGen`.
+-- why doesn't the type signature of `next` include a type constraint?
+-- `random :: (RandomGen g, Random a) => g -> (a, g)`
+-- A way to generate random values that aren't numbers.
+-- The range generated is determined by the type.
+
+fooGen :: StdGen
+fooGen = mkStdGen 0
+
+-- the random number generation here is deterministic
+-- given the same generator, we'll generate the same random number every time
+barRandom1 :: (Int, StdGen)
+barRandom1 = next fooGen
+-- (2147482884,40014 40692)
+
+barRandom2 :: (Int, StdGen)
+barRandom2 = next fooGen
+-- (2147482884,40014 40692)
+
+-- `random` is used to generate values of different types
+bazRandom1 :: (Char, StdGen)
+bazRandom1 = random fooGen
+
+bazRandom2 :: (Char, StdGen)
+bazRandom2 = random fooGen
+
+-- randomR is used to generate a random number within a range
+-- (RandomGen g, Random a) => (a, a) -> g -> (a, g)
+
+quxRandom1 :: (Int, StdGen)
+quxRandom1 = randomR (0, 3) fooGen
+-- (3,40014 40692)
+
+-- to get a new random value, you have to pass to use the new StdGen returned 
+-- with the last random value
+
+-- The State newtype can help with this
+
+-- newtype State s a = State { runState :: s -> (a, s) }
+
+-- newtype Reader r a = Reader { runReader :: r -> a }
+
+-- a newtyp emust have the same underlying representation as the type they wrap
+-- a function contained in the newtype must be isomorphic to the type it wraps
+-- an isomorphism is the ability to go from the newtype to the value it wraps
+-- and back to the newtype without losing information
+
+type Iso a b = (a -> b, b -> a)
+
+newtype Sum a = Sum { getSum :: a }
+
+sumIsIsomorphicWithItsContents :: Iso a (Sum a)
+sumIsIsomorphicWithItsContents = (Sum, getSum)
+-- the Sum data constructor is (a -> Sum a)
+-- the getSum function is (Sum a -> a)
+-- that is an isomorphism
+
+-- Is not an isomorphism: (a -> Maybe b, b -> Maybe a)
+-- if you go from a -> Nothing, there is no way to get back to a. the information
+-- in a is lost
