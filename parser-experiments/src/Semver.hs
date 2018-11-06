a8500c21d2d7b8f36825bdd38c261fa746927048
Max Hallinan <maxhallinan@riseup.net>
2018-11-06 21:07:56 +0100

    Start working on semantic version prerelease segment parser

--- parser-experiments/src/Semver.hs
+++ parser-experiments/src/Semver.hs
@@ -1,8 +1,14 @@
 module Semver where
 
+import Control.Applicative ((<|>))
+import Data.Foldable
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
+
+data Identifier = IntegerId Integer | StringId String deriving (Eq, Ord, Show)
+
+data PreRelease = PreRelease [Identifier] deriving (Eq, Ord, Show)
 
 newtype Patch = Patch Integer deriving (Eq, Ord, Show)
 
@@ -17,7 +23,43 @@ instance Ord Version where
     (major1 <= major2) && (minor1 <= minor2) && (patch1 <= patch2)
 
 type Parser = Parsec Void String
-  
+
+-- A pre-release version MAY be denoted by appending a hyphen and a series of 
+-- dot separated identifiers immediately following the patch version. 
+-- Identifiers MUST comprise only ASCII alphanumerics and hyphen [0-9A-Za-z-]. 
+-- Identifiers MUST NOT be empty. Numeric identifiers MUST NOT include leading 
+-- zeroes. Pre-release versions have a lower precedence than the associated 
+-- normal version. A pre-release version indicates that the version is unstable 
+-- and might not satisfy the intended compatibility requirements as denoted by 
+-- its associated normal version. Examples: 1.0.0-alpha, 1.0.0-alpha.1, 
+-- 1.0.0-0.3.7, 1.0.0-x.7.z.92.
+
+identifierParser :: Parser Identifier
+identifierParser = integerIdParser <|> stringIdParser
+  where
+    integerIdParser = (IntegerId . read) <$> some digitChar
+    stringIdParser  = StringId <$> some alphaNumChar
+
+segmentParser :: Parser [Identifier]
+segmentParser = some identifierParser
+
+-- Question:
+-- 1. See if there is a '-' character immediately after the patch version.
+-- 2. If there is, then parse what follows until I hit a '+'.
+-- 3. If '+' is found, use the metadata parser.
+-- 4. Don't fail if neither are found.
+-- 5. Don't fail if only one is found.
+
+-- Question:
+-- 1. How do I allow for an unbounded pattern of identifier followed by dot where
+--    there is no dot at the beginning or the end of the segment?
+
+preReleaseParser :: Parser PreRelease
+preReleaseParser = do
+  char '-'
+  segments <- many (dotParser >> segmentParser)
+  return $ PreRelease (fold segments)
+
 integerParser :: Parser Integer
 integerParser = read <$> some digitChar
 
@@ -40,6 +82,7 @@ versionParser = do
   minor <- minorParser
   dotParser
   patch <- patchParser
+  preRelease <- preReleaseParser
   eof
   return $ Version major minor patch
 
